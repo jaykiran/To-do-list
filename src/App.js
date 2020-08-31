8e9c567c460f6c095eafefc6966f7b14e8c5046e
jaykiran <jaikiranjay@gmail.com>
2020-08-31 23:58:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ListInput from './Components/ListInput';
+import ToDoList from './Components/ToDoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state={
+    items:[{id:1, title:'wake up'}, {id:2, title:'Scrum call'}],
+    id:uuidv4(),
+    item: '',
+    editItem: false
+  };
+  handleChange = (e)=>{
+    console.log('handle change');
+  };
+
+  handleSubmit = (e)=>{
+    console.log('handle Submit');
+  };
+  clearList = () => {console.log('clear List')};
+  handleDelete = (id) =>{
+    console.log(`handle edit ${id}`);
+  };
+  handleEdit = (id) =>{
+    console.log(`edit ${id}`);
+  } ;
+
+  render(){
+    return (
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt-5">
+              <h3 className="text-capitalize text-center">ToDo Input</h3>
+              <ListInput 
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              />
+              <ToDoList
+                items={this.state.items}
+                clearList={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+              />
+            </div>
+          </div> 
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/ToDoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ListItem from './ListItem';
+
+export default class ToDoList extends Component {
+    render() {
+        return (
+            <div>
+                Hello from todo list
+            </div>
+        )
+    }
+}
